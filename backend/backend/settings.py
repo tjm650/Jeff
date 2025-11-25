bd4f92923a8aa2d3c98649d494b81eb081417bf2
tjm650 <tinotendajenya.org@gmail.com>
2025-11-25 20:37:09 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,15 +151,10 @@ ASGI_APPLICATION = 'backend.asgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Cache configuration (using in-memory for development)
