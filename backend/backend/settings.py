a240a179d3902f6c02dc23cf92a1cfd8e433a20f
tjm650 <tinotendajenya.org@gmail.com>
2025-11-25 20:14:43 +0200

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','.fly.dev').split(',')
 
 # CORS configuration
 CORS_ALLOWED_ORIGINS = []
